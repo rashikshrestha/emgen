28173e925b29db17d130b244aaef951ad552862d
rashikshrestha <rashikshrestha01@gmail.com>
2025-04-04 16:24:29 -0400

    mnist dataset config added

--- emgen_config/dataset/dataset.py
+++ emgen_config/dataset/dataset.py
@@ -5,3 +5,8 @@ class ToyDatasetConfig:
     _target_: str = "emgen.dataset.toy_dataset.ToyDataset"
     name: str = "dino"
     num: int = 8000
+    
+@dataclass
+class MNISTDatasetConfig:
+    _target_: str = "emgen.dataset.mnist.MNISTDataset"
+    train: bool = True
