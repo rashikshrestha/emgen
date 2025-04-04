9dd6701084824cc8771a262b52029c8e74afe247
rashikshrestha <rashikshrestha01@gmail.com>
2025-04-04 06:55:17 -0400

    single dataset extraction in test_mnist

--- tests/dataset/test_mnist.py
+++ tests/dataset/test_mnist.py
@@ -6,7 +6,8 @@ from torchvision.utils import save_image
 def test_mnist():
     dataset = MNISTDataset()
     assert len(dataset)==60000
-    assert dataset[0].shape == torch.Size([1, 28, 28])
-    assert dataset[0].dtype == torch.float32
-    assert dataset[0].min() == 0 
-    assert dataset[0].max() == 1
+    data = dataset[0]
+    assert data.shape == torch.Size([1, 28, 28])
+    assert data.dtype == torch.float32
+    assert data.min() == 0 
+    assert data.max() == 1
