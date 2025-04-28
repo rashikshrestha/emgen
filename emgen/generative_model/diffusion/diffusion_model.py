ee3982f906e7ae1e89a0b74904f7da10aebfe1fc
rashikshrestha <rashikshrestha01@gmail.com>
2025-04-27 23:12:38 -0400

    toy data experiments updated

--- emgen/generative_model/diffusion/diffusion_model.py
+++ emgen/generative_model/diffusion/diffusion_model.py
@@ -12,11 +12,12 @@ from pathlib import Path
 from hydra.core.hydra_config import HydraConfig
 
 #! emgen imports
-from emgen.utils.visualization import plot_2d_intermediate_samples, plot_images_intermediate_samples
+from emgen.utils.visualization import plot_2d_intermediate_samples, plot_images_intermediate_samples, plot_kl_divergences, plot_timeseries_1d_pdf
 from emgen.generative_model.diffusion.noise_scheduler import NoiseScheduler
 from emgen.generative_model.diffusion.diffusion_model_arch import LinearArch
 from emgen.dataset.toy_dataset import ToyDataset
 from emgen.utils.config_schema import TrainConfig
+from emgen.utils.metrics import compute_Nd_kl_divergence, project_to_lower_dim
 
 log = logging.getLogger(__name__)
 
@@ -37,7 +38,10 @@ class DiffusionModel():
         self.device = device
         self.noise_scheduler = noise_scheduler
         self.diffusion_arch = diffusion_arch.to(self.device)
+        self.dataset = dataset
         self.train_config = train
+        self.train_length = len(dataset)
+        self.no_of_eval_batches = int(self.train_length/self.train_config.eval_batch_size)
         
         self.out_dir = Path(HydraConfig.get().runtime.output_dir)
         self.data_dim = dataset[0].shape
@@ -90,32 +94,67 @@ class DiffusionModel():
                 nn.utils.clip_grad_norm_(self.diffusion_arch.parameters(), 1.0)
                 self.optimizer.step()
                 self.optimizer.zero_grad()
-                
+               
+            #! If this is 'save_images' epoch or the last epoch
             if epoch % self.train_config.save_images_step == 0 or epoch == self.train_config.num_epochs - 1:
-                sample_out = self.sample()
-                sample_out_dir = self.out_dir/f"train_epoch_{epoch:02d}"
-                sample_out_dir.mkdir(parents=True, exist_ok=True)
+                print("generating samples...")
+                all_generated_samples = []
+                for eb in tqdm(range(self.no_of_eval_batches)):
+                    sample_out = self.sample()
+                    all_generated_samples.append(sample_out['intermediate_samples'])
+                    
+                    if eb==0:
+                        sample_out_dir = self.out_dir/f"train_epoch_{epoch:02d}"
+                        sample_out_dir.mkdir(parents=True, exist_ok=True)
+                        
+                        #! Plotting
+                        # If Image Data
+                        if len(sample_out['intermediate_samples'].shape) == 5:
+                            plot_images_intermediate_samples(
+                                sample_out['intermediate_samples'], 
+                                sample_out_dir, 
+                                self.train_config.no_of_diff_samples_to_save,     
+                            )
+                        # If 2D Data
+                        elif len(sample_out['intermediate_samples'].shape) == 3:
+                            plot_2d_intermediate_samples(
+                                sample_out['intermediate_samples'], 
+                                sample_out_dir, 
+                                self.train_config.no_of_diff_samples_to_save,     
+                            )
+                        torch.save(self.diffusion_arch.state_dict(), self.out_dir/f"train_epoch_{epoch:02d}/diffusion_arch.pth")
+                        
+                all_generated_samples = np.concatenate(all_generated_samples, axis=1)
+                all_generated_samples = torch.as_tensor(all_generated_samples)
+               
+                all_kl = self.compute_kl(all_generated_samples, self.dataset.data) 
                 
-                #! Plotting
-                # If Image Data
-                if len(sample_out['intermediate_samples'].shape) == 5:
-                    plot_images_intermediate_samples(
-                        sample_out['intermediate_samples'], 
-                        sample_out_dir, 
-                        self.train_config.no_of_diff_samples_to_save,     
-                    )
-                # If 2D Data
-                elif len(sample_out['intermediate_samples'].shape) == 3:
-                    plot_2d_intermediate_samples(
-                        sample_out['intermediate_samples'], 
-                        sample_out_dir, 
-                        self.train_config.no_of_diff_samples_to_save,     
-                    )
-                torch.save(self.diffusion_arch.state_dict(), self.out_dir/f"train_epoch_{epoch:02d}/diffusion_arch.pth")
+                timesteps = list(range(all_kl.shape[0]))[::-1]
+                timesteps = np.array(timesteps)
+                
+                plot_kl_divergences(
+                    all_kl, timesteps, output_file=sample_out_dir/"kl_divergences.png", 
+                    title = f"Epoch {epoch:02}: KL Divergences between GT and Generated Samples",
+                    x_label="Timesteps", invert_x=True
+                )
+               
+                all_generated_samples_1d, gt_samples_1d = project_to_lower_dim(all_generated_samples, self.dataset.data, out_dim=1)
+                all_generated_samples_1d, gt_samples_1d = all_generated_samples_1d.squeeze(), gt_samples_1d.squeeze()
+                
+                
+                samples_to_plot = np.vstack((all_generated_samples_1d, gt_samples_1d))
+                timesteps = np.concatenate((timesteps, np.array([0])))
+                
+                
+                plot_timeseries_1d_pdf(samples_to_plot, timesteps,
+                    title=f"Epoch {epoch:02}: Evolution of PDF during Reverse Diffusion",
+                    figsize=(8, 5),
+                    save_path=sample_out_dir/"pdf_1d_evolution.png"
+                )
 
  
     def sample(self, get_intermediate_samples=True):
-        log.info("Sampling from the Diffusion Model")
+        # log.info("Sampling from the Diffusion Model")
         self.diffusion_arch.eval()
         #! Accumulators
         output = {}
@@ -125,11 +164,11 @@ class DiffusionModel():
         # from emgen.dataset.toy_dataset import get_gt_dino_dataset
         # sample = get_gt_dino_dataset()
         # sample = torch.as_tensor(sample, device=self.device, dtype=torch.float32)
-        if get_intermediate_samples: intermediate_samples.append(sample.cpu().numpy())
+        if get_intermediate_samples: intermediate_samples.append(sample.cpu().numpy()) # Initial noise
         #! List the timesteps in reverse order for sampling
         timesteps = list(range(len(self.noise_scheduler)))[::-1]
         #! Reverse diffusion process
-        for i, t in enumerate(tqdm(timesteps)):
+        for i, t in enumerate(timesteps):
             t = torch.from_numpy(np.repeat(t, self.train_config.eval_batch_size)).long().to(self.device)
             # t = torch.from_numpy(np.repeat(t, 142)).long().to(self.device)
             with torch.no_grad():
@@ -141,3 +180,26 @@ class DiffusionModel():
         generated_sample = sample.cpu().numpy()
         output['generated_sample'] = generated_sample
         return output
+    
+    
+    def compute_kl(self, all_gen_samples, gt_samples):
+        """
+        Compute KL divergence between the GT samples and all generated samples
+        
+        Args:
+            gt_samples (torch.Tensor): Ground truth samples (N,d)
+            all_gen_samples (torch.Tensor): All generated samples (T,N,d)
+        """
+        #! Convert to numpy arrays if they are PyTorch tensors
+        if isinstance(gt_samples, torch.Tensor):
+            gt_samples = gt_samples.detach().cpu().numpy()
+        if isinstance(all_gen_samples, torch.Tensor):
+            all_gen_samples = all_gen_samples.detach().cpu().numpy()
+        
+        #! Compute KL divergence
+        all_kl = []
+        for gen_samples in all_gen_samples:
+            kl_div = compute_Nd_kl_divergence(gen_samples, gt_samples)
+            all_kl.append(kl_div)
+            
+        return np.array(all_kl)
