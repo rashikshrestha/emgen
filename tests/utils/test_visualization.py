72e95114f87e90d982c975419cf21a5f454a0171
rashikshrestha <rashikshrestha01@gmail.com>
2025-04-27 23:11:24 -0400

    test plot tieseries 1d updated

--- tests/utils/test_visualization.py
+++ tests/utils/test_visualization.py
@@ -3,18 +3,20 @@ from emgen.utils.visualization import *
 
 def test_plot_timeseries_1d_pdf():
     # Create dummy diffusion samples
-    timesteps = list(range(0, 100))  # 10 timesteps
+    timesteps = torch.tensor(list(range(0, 100))[::-1])  #  timesteps
     
     diffusion_samples = []
     for t in timesteps:
         # Generate samples whose mean slowly moves from 0 to 5 and variance increases
         mean = t * 0.5
         std = 0.5 + 0.1 * t
-        samples = torch.normal(mean=mean, std=std, size=(1000, 1))
+        samples = torch.normal(mean=mean, std=std, size=(1000,))
         diffusion_samples.append(samples)
+        
+    diffusion_samples = torch.stack(diffusion_samples, dim=0)  # Shape: (100, 1000)
     
     # Call the function
-    fig = plot_timeseries_1d_pdf(diffusion_samples, timesteps,
+    fig = plot_timeseries_1d_pdf(diffusion_samples, timesteps, invert_x=True,
                                  title="Test Plot: Diffusion Evolution",
                                  figsize=(8, 5),
                                  save_path='this.png'
@@ -27,4 +29,4 @@ def test_plot_timeseries_1d_pdf():
     print("Test passed successfully!")
     
     
-test_plot_timeseries_1d_pdf()
+# test_plot_timeseries_1d_pdf()
